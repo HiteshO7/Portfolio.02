ed7f8f9c8925231939215a139d5a6716dff8474d
Hitesh Thakur <123528848+HiteshO7@users.noreply.github.com>
2024-09-11 10:53:37 +0530

    Update SlidingImages.jsx

--- src/components/SlidingImages/SlidingImages.jsx
+++ src/components/SlidingImages/SlidingImages.jsx
@@ -41,7 +41,7 @@ const slider2 = [
     }
 ]
 
-export default function slidingImages() {
+export default function SlidingImages() {
 
     const container = useRef(null);
     const { scrollYProgress } = useScroll({
