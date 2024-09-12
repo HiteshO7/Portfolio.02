00d5328473f0f1c32fe4e1103bd45cf68dbb443d
Hitesh Thakur <hiteshthakur695#gmail.com>
2024-09-12 09:47:49 +0530

    Changes made

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,10 @@
-import './globals.css'
-import Header from '../components/Header/Header'
+import './globals.css';
+import Header from '../components/Header/Header';
 
 export const metadata = {
   title: 'Portfolio',
   description: '',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -14,11 +14,12 @@ export default function RootLayout({ children }) {
         <meta name="description" content={metadata.description} />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/bgg.jpg" />
         <link rel="apple-touch-icon" href="/images/bgg.jpg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> {/* Add this line */}
       </head>
       <body>
         <Header />
         {children}
       </body>
     </html>
-  )
+  );
 }
